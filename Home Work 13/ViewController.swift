d2d13991fbb0e62600b0883609fa344031d69100
Artem Derbin <artemsdft@mail.ru>
2022-05-04 11:59:33 +0300

    Initial Commit

---
+++ Home Work 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 13
+//
+//  Created by Артем Дербин on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
